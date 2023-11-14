bfa9dd05222e9815d7d5394acae4c66d7cff9cd7
Watson <nya.dawson@abbott.com>
2023-11-14 12:17:10 -0500

    third commit

--- src/src.py
+++ src/src.py
@@ -1,1 +1,11 @@
 #main source file
+def add(a, b):
+    return a + b
+
+
+def say_hello():
+    print("hello world!")
+
+
+if __name__ == '__main__':
+    say_hello()
--- test/test_main.py
+++ test/test_main.py
@@ -1,1 +1,13 @@
 # main test file
+import unittest
+from src import main
+
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        assert main.add(2, 3) == 5
+        print("Add Function works correctly")
+
+
+if __name__ == '__main__':
+    unittest.main()
